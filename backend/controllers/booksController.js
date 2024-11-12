cbb3d81623cfec7062095d3456950d38b316cb8a
xiomary <Xiomary331@gmail.com>
2024-11-12 18:14:06 -0500

    Initial Changes

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -11,6 +11,7 @@ const getAllBooks = async (req, res) => {
 const getBook = async (req, res) => {
   const { id } = req.params; // Extracting the book ID from the request parameters
   // second comment 
+  //third comment
   try {
     const book = await Book.findById(id); // Fetch the book from the database
 
