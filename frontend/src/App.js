5c6effc6cfd715d04a0f664b2a2a5dd642d0e1ee
xiomary <Xiomary331@gmail.com>
2024-12-03 18:08:46 -0500

    Added signup route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CreateBook from './CreateBook';
+import SignUp from './SignUp';
 
 function App() {
   return (
-
+    <Router>
       <div className="App">
-        <Navbar /> 
+        <Navbar />
         <div className="pages">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreateBook />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
         </div>
       </div>
-    
+    </Router>
   );
 }
 
