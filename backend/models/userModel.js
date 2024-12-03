395915baccdb4b631046de58b403e3b73e818477
xiomary <Xiomary331@gmail.com>
2024-12-03 18:16:23 -0500

    Removed username field

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,45 +1,38 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose; 
-const { isEmail } = require('validator');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  },
   email: {
     type: String,
-    required: [true, 'Please enter an email'],
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
-    validate: [isEmail, 'Please enter a valid email']
+    validate: [isEmail, "Please enter a valid email"],
   },
   password: {
     type: String,
-    required: [true, 'Please enter a password'],
-    minlength: [6, 'Minimum password length is 6 characters']
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 characters"],
   },
   isAdmin: {
     type: Boolean,
     required: false,
-  }
+  },
 });
 
-// Pre-save middleware to hash the password before saving
+// function called before doc saves to the db
 
-userSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  }
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-userSchema.post('save', function(doc, next) {
-  console.log(`User ${doc.username} has been saved successfully.`);
+userSchema.post("save", function (doc, next) {
+  console.log(`Use has been saved successfully.`);
   next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
