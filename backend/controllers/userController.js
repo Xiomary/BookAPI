ed5eb3e91c28120d658c3ee01a25f66a3e7150bd
xiomary <Xiomary331@gmail.com>
2024-12-10 19:52:39 -0500

    hashing password and generating jwt token for user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,32 +1,63 @@
+const { HttpStatusCode } = require("axios");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const hashPassword = require('../utils/passwordUtils'); 
+//const createToken = require('../utils/tokenUtils');
 
+// // handle errors
+// const handleErrors = (err) => {
+//   console.log(err.message, err.code);
+//   let errors = { email: "", password: "" };
 
-// Handle errors
-const handleErrors = (err) => {
-  console.log(err.message, err.code);
-  let error = {email: '', password: ''};
+//   // Log validation errors if they exist
+//   if (err.message.includes("user validation failed")) {
+//     Object.values(err.errors).forEach((properties) => {
+//       errors[properties.path] = properties.message;
+//     });
+//   }
+//   return errors;
+// };
 
-  // Log validation errors if they exist
-  if (err.message.includes('user validation failed')) {
-    Object.values(err.errors).forEach(error => {
-      console.log(errors.properties);
-    })
-  }
+// create token
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: maxAge
+  });
 }
-
 
 // Create a new user
 const createUser = async (req, res) => {
-  const { username, email, password } = req.body;
   try {
-    const user = await User.create({
-      username: username,
-      email: email,
-      password: password,
-    });
-    res.status(200).json(user);
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email) return res.status(400).send("Invalid email");
+    if (!password || password.length < 6) return res.status(400).send("Password is required and should be at least 6 characters");
+
+    // Check if user already exists
+    const userExist = await User.findOne({ email });
+    if (userExist) return res.status(400).send("Email is already taken");
+
+    // Hash the password
+    const hashedPassword = await hashPassword(password);
+
+    // Create and save the new user
+    const user = new User({ email, password: hashedPassword });
+    await user.save();
+
+    console.log("Saved user", user);
+    
+
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000});
+    console.log(res.getHeaders()['set-cookie']); 
+   // Send back a response
+    res.status(201).json({ message: "User created successfully", userId: user._id });
+
   } catch (error) {
-    res.status(404).json({ message: "Failed to create user" });
+    console.log("Error", error);
+    return res.status(500).send("Error. Try again");
   }
 };
 
@@ -47,7 +78,7 @@ const getUser = async (req, res) => {
   try {
     const user = await User.findById(userId);
     if (user) {
-      res.status(200).json({user});
+      res.status(200).json({ user });
     }
   } catch (error) {
     res.status(400).json({ message: "User not found" });
@@ -55,39 +86,37 @@ const getUser = async (req, res) => {
 };
 
 // Delete user
-const deleteUser = async(req,res) => {
-    const userId = req.params.id;
-    try {
-        const user = await User.findById(userId);
-        if(!user) {
-            return res.status(400).json({message: 'User not found'})
-        }
-        await User.findByIdAndDelete(userId);
-        res.status(200).json({message: 'User deleted successfully'});
-    }catch(error) {
-        res.status(500).json({message: 'Could not delete user'});
+const deleteUser = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
-}
+    await User.findByIdAndDelete(userId);
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Could not delete user" });
+  }
+};
 
 // Update user
-const updateUser = async(req,res) => {
-    const userId = req.params.id;
-    const {username, email, password} = req.body;
-    try{
-        const user = await User.findById(userId);
-        if(!user) {
-            return res.status(404).json({message: "User not found"});
-        }
-        user.username = username;
-        user.email = email;
-        user.password = password;
-        await user.save();
-        res.status(200).json({message: 'User updated succesfully'});
-        
-    }catch(error) {
-        res.status(500).json({message: "Server error"});
+const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const { email, password } = req.body;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+    user.email = email;
+    user.password = password;
+    await user.save();
+    res.status(200).json({ message: "User updated succesfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 module.exports = {
   createUser,
