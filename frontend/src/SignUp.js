dc4d3f3501e9d9ec95dec33eb52eb980fce3c9e4
xiomary <Xiomary331@gmail.com>
2024-12-14 15:05:43 -0500

    added navigation to sign up page

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
 import './index.css'; 
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e) => { 
     e.preventDefault();
     axios.post('http://localhost:8081/api/users/signup', {
       email,
       password,
-    }).then((response) => {
-      console.log(response);
+    }, {
+      withCredentials: true, 
     })
+    .then((response) => {
+      console.log(response); 
+      if(response.data.message === 'User created successfully') {
+        navigate("/login");
+      }else {
+        console.log("Unexpected response data:", response.data);
+        navigate("/home");
+      }
+    })
+    .catch((error) => console.log(error)); 
   };
 
   return (
     <div className="signup-container">
       <form onSubmit={handleSubmit} className="signup-form">
-      <h2>Sign Up</h2>
+        <h2>Sign Up</h2>
         <input
           type="email"
           placeholder="Enter email"
